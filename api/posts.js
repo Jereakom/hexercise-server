6ab1e3a4185f64dfb76f2dac981e5aaa5ddc0ba8
root <jereakom@gmail.com>
2016-10-04 07:14:28 +0300

    Fixed server post

--- api/posts.js
+++ api/posts.js
@@ -1,9 +1,35 @@
+require('dotenv').config();
 var express = require('express');
 var connection = require('../modules/newconnect.js');
 var posts = express();
-var result = connection.select();
+var pgp = require("pg-promise")(/*options*/);
+
+var conn = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  ssl: 'true'
+};
+
+var db = pgp(conn);
+
+var port = process.env.PORT || 80
+
+
 posts.get('/api/posts', function(req, res) {
-  res.send(result);
+
+  db.one('select * from posts;')
+  .then(function (data) {
+        res.status(200)
+          .json({
+            data: data
+          });
+      })
+      .catch(function (err) {
+        return next(err);
+      });
 });
 
 posts.get('/api/posts/search', function(req, res) {
@@ -13,6 +39,6 @@ posts.get('/api/posts/:id', function(req, res) {
   res.send("posts get, with id "+req.params.id);
 });
 
-posts.listen(80, function () {
+posts.listen(port, function () {
   console.log('Example app listening on port 80!');
 });
