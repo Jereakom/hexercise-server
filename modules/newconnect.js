45b9ac7abe3b716152c2f5c6fef69850d2014c1d
root <jereakom@gmail.com>
2016-10-04 07:34:41 +0300

    Database connection

--- modules/newconnect.js
+++ modules/newconnect.js
@@ -2,16 +2,17 @@ require('dotenv').config();
 
 var pgp = require("pg-promise")(/*options*/);
 
-var conn = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    ssl: 'true'
-  };
+// var conn = {
+//
+//     // host: process.env.DB_HOST,
+//     // port: process.env.DB_PORT,
+//     // database: process.env.DB,
+//     // user: process.env.DB_USER,
+//     // password: process.env.DB_PWD,
+//     ssl: 'true'
+//   };
 
-var db = pgp(conn);
+var db = pgp('postgres://kiivbqkqjrmudf:4RMK_1Xk2rekkD23S4r91PhciW@ec2-54-75-228-83.eu-west-1.compute.amazonaws.com:5432/del8prb26rjs6r?ssl=true');
 
 db.any('select * from posts;')
 .then(function (data) {
